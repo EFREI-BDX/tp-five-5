31ce4e1649311484811bd569f84e231a1fcd90fb
Hugo Leyx-valade <hleyx-valade@hellowork.com>
2026-03-30 16:31:06 +0200

    feat: spring initializer

---
+++ repo-root/groups/group-01-manage-field/code/managefield/src/test/java/com/jad/efreifive/managefield/ManagefieldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jad.efreifive.managefield;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagefieldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
