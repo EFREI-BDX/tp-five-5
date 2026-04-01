777443fc52cad0386b5c3223cff41f9780724cae
Jean-Aymeric Diet <jeanaymeric@gmail.com>
2026-04-01 17:21:23 +0200

    feat: update SQL schema to include idTeamLeader in team and modify foreign key references

--- repo-root/groups/group-06-manage-team/sql/team-create.sql
+++ repo-root/groups/group-06-manage-team/sql/team-create.sql
@@ -10,6 +10,7 @@ CREATE TABLE fiveteam.team
     tag             varchar(3)           not null,
     creationDate    date                 not null,
     dissolutionDate date                 null,
+    idTeamLeader    binary(16)           null,
     state           enum ('A', 'I', 'D') not null
 );
 
@@ -17,9 +18,12 @@ CREATE TABLE fiveteam.player
 (
     id          binary(16)   not null primary key,
     displayName varchar(255) not null,
-    teamId      binary(16)   null,
-    foreign key (teamId) references fiveteam.team (id)
+    idTeam      binary(16)   null,
+    foreign key (idTeam) references fiveteam.team (id)
 );
+
+ALTER TABLE fiveteam.team
+    ADD CONSTRAINT team_leader_fk FOREIGN KEY (idTeamLeader) REFERENCES fiveteam.player (id);
 
 CREATE VIEW fiveteam.TeamView AS
 SELECT `fiveteam`.`team`.`id`              AS `id`,
@@ -27,13 +31,14 @@ SELECT `fiveteam`.`team`.`id`              AS `id`,
        `fiveteam`.`team`.`tag`             AS `tag`,
        `fiveteam`.`team`.`creationDate`    AS `creationDate`,
        `fiveteam`.`team`.`dissolutionDate` AS `dissolutionDate`,
+       `fiveteam`.`team`.`idTeamLeader`    AS `idTeamLeader`,
        `fiveteam`.`team`.`state`           AS `state`
 FROM `fiveteam`.`team`;
 
 CREATE VIEW fiveteam.PlayerView AS
 SELECT `fiveteam`.`player`.`id`          AS `id`,
        `fiveteam`.`player`.`displayName` AS `displayName`,
-       `fiveteam`.`player`.`teamId`      AS `teamId`
+       `fiveteam`.`player`.`idTeam`      AS `idTeam`
 FROM `fiveteam`.`player`;
 
 GRANT SELECT ON fiveteam.TeamView TO 'jad_efrei_five_2526'@'%';
