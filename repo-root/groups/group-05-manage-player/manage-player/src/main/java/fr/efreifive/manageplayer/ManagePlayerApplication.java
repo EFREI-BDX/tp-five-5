c8cdaa9d30a0df467318314091f9f52069aaf1f1
aluxey <aurelien.luxeyy@gmail.com>
2026-04-02 16:36:29 +0200

    add Spring Boot bootstrap for manage-player API

---
+++ repo-root/groups/group-05-manage-player/manage-player/src/main/java/fr/efreifive/manageplayer/ManagePlayerApplication.java
@@ -0,0 +1,11 @@
+package fr.efreifive.manageplayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagePlayerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ManagePlayerApplication.class, args);
+    }
+}
