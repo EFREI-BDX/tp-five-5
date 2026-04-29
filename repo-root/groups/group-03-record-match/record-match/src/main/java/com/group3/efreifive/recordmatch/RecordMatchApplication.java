004b67ea91b5a361cb0bbbfa9207fdb8a7926136
Bussy <olubusolaogoe@gmail.com>
2026-04-29 18:10:54 +0200

    chore: initialisation du projet java

---
+++ repo-root/groups/group-03-record-match/record-match/src/main/java/com/group3/efreifive/recordmatch/RecordMatchApplication.java
@@ -0,0 +1,13 @@
+package com.group3.efreifive.recordmatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecordMatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecordMatchApplication.class, args);
+	}
+
+}
---
+++ repo-root/groups/group-03-record-match/record-match/src/test/java/com/group3/efreifive/recordmatch/RecordMatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group3.efreifive.recordmatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecordMatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
