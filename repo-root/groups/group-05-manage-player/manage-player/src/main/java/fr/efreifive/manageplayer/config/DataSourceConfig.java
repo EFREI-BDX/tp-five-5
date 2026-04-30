4e9f18a8f00a2fbd2069ba25d8ac10dad30db93f
aluxey <aurelien.luxeyy@gmail.com>
2026-04-30 13:52:23 +0200

    Branche l API sur MariaDB en JDBC

---
+++ repo-root/groups/group-05-manage-player/manage-player/src/main/java/fr/efreifive/manageplayer/config/DataSourceConfig.java
@@ -0,0 +1,32 @@
+package fr.efreifive.manageplayer.config;
+
+import javax.sql.DataSource;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+@Configuration
+@ConditionalOnProperty("spring.datasource.url")
+public class DataSourceConfig {
+    @Bean
+    @Primary
+    @ConfigurationProperties("spring.datasource")
+    public DataSourceProperties dataSourceProperties() {
+        return new DataSourceProperties();
+    }
+
+    @Bean
+    @Primary
+    public DataSource dataSource(DataSourceProperties dataSourceProperties) {
+        return dataSourceProperties.initializeDataSourceBuilder().build();
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
+}
